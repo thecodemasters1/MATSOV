88a42c10b15d01fda5d352fe1afbf797f658011d
thecodemasters1 <thecodemasters1@users.noreply.github.com>
2015-09-22 20:29:19 +0300

    Update Drink.cpp

--- PCE/CPP Chapter/freeOnTheBar/Drink.cpp
+++ PCE/CPP Chapter/freeOnTheBar/Drink.cpp
@@ -5,8 +5,8 @@ const char* STRING_NOT_FOUND_EXCEPTION = "String not found exception";
 
 Drink::Drink(const char* name)
 {
-	m_name = new char[100];
-	strcpy(m_name, name);
+	m_name = new char[100]; // T: 100 magic number, put it in define
+	strcpy(m_name, name); // T: can cause buffer overflow, use strncpy, check m_name is not null
 }
 
 Drink::~Drink()
@@ -16,7 +16,7 @@ Drink::~Drink()
 
 char* Drink::getName() const
 {
-	if(m_name == NULL) { throw STRING_NOT_FOUND_EXCEPTION; }
+	if(m_name == NULL) { throw STRING_NOT_FOUND_EXCEPTION; } // T: use specific exception, no reason it will be null
 	return m_name;
 }
 
