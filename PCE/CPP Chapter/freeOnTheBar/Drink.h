ea9d8ff3b3fe1de04757bf328fde21f304ed9eaf
thecodemasters1 <thecodemasters1@users.noreply.github.com>
2015-09-22 20:34:01 +0300

    Update Drink.h

--- PCE/CPP Chapter/freeOnTheBar/Drink.h
+++ PCE/CPP Chapter/freeOnTheBar/Drink.h
@@ -7,7 +7,7 @@ public:
 	Drink(const char* name);
 	virtual ~Drink();
 	virtual char* getName() const;
-	virtual void prepare() const = 0;
+	virtual void prepare() const = 0; // T: better prepare return a string and not print to std
 
 protected:
 	char* m_name;
